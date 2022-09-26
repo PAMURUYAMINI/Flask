fe9cc34f216fde55b75de7da40de14cf02b14041
PAMURUYAMINI <105670582+PAMURUYAMINI@users.noreply.github.com>
2022-09-26 21:27:21 +0530

    Flask

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('/index.html')
+@app.route("/")
+def hello_world():
+    return render_template('home.html', name="Home")
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return render_template('about.html')
+  return render_template('about.html', name="About")
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
 
-@app.route('/signin')
-def signin():
-    return render_template('signin.html')
+@app.route("/login")
+def login():
+  return render_template('login.html', name="Login")
+
+@app.route("/register")
+def register():
+  return render_template('register.html', name="Register")
+
